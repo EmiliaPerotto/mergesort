da80b183a31f1a89b7783f6509f35a066f2987fb
Emilia Perotto <100068661@csisd.org>
2025-01-24 14:51:49 -0600

    kgg

--- MergeSort.java
+++ MergeSort.java
@@ -1,8 +1,15 @@
 import java.io.*;
 import java.util.*;
 public class MergeSort{
-  public static int[] merge(int[] arr1, int[]arr2){
+  public static int[] merge_split (int[] arr1, int low, int high){
+    int middle = (low + high) / 2;
+    return middle;
+    int[] arr_right = new int[middle];
+    int[] arr_left = new int[middle];
+    int right = middle;
+    int 
     
+
   }
   public static void main (String args[]) throws IOException{
     Scanner in = new Scanner(System.in);
@@ -16,6 +23,8 @@ public class MergeSort{
       arr[j] = fin.nextInt();
       j++;
     } 
+    int low = 0;
+    int high = arr.length -1;
     for (int i = 0; i < arr.length; i++){
       if(i != arr.length -1){
         System.out.print(arr[i] + " , ");
@@ -24,5 +33,6 @@ public class MergeSort{
         System.out.print(arr[i]);
     }
   }
+      //System.out.println("Middle val: " + merge_split(arr, low, high));    this works
   }
 }
